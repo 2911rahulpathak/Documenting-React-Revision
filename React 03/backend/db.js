0a3ed015dcdc816ff27eb14b8b3c53bb2fe6e787
Rahul <prathampathak14@gmail.com>
2024-01-08 02:47:00 +0530

    changes made

--- React 03/backend/db.js
+++ React 03/backend/db.js
@@ -1,8 +1,7 @@
 //mongoose is the library we are using to connect to the database
 const mongoose = require("mongoose");
-// mongodb+srv://admin:123admin@cluster0.sgj7wka.mongodb.net/
 
-mongoose.connect("mongodb+srv://admin:123admin@cluster0.sgj7wka.mongodb.net/");
+mongoose.connect("");
 //create a schema
 const todoSchema = mongoose.Schema({
   title: String,
