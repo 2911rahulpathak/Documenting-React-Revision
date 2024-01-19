aa80467fb9673deb924686848a72d5112d1fcd2a
Rahul <prathampathak14@gmail.com>
2024-01-20 00:17:43 +0530

    learnings06

--- React 05/Hooks/src/App.jsx
+++ React 05/Hooks/src/App.jsx
@@ -1,38 +1,39 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import { memo } from "react";
 import { useEffect } from "react";
 
 function App() {
 const [counter, setCounter] = useState(0);
-  const [inputValue, setInputValue] = useState(1);
-  const [count,setCount] = useState(0);
+  //when passed variable it's not changing as the value is not changed
+ // var a = 1;
+ //If we change it to function It will think that Its new function & there is diff as new refernce of function is created
+/* function a(){
+  console.log("hi there")
+ }*/
+//Using callBack solve this 
+//Instead of defining the function in way func a(){}
+//define it like this:
+const a = useCallback(function(){
+  console.log("hi there")
+},[])
+ //IF we dont use Callback It causes unnecessary re-renders
 
-  //here can't we just remember the value from last operation
-  /*As on every button click It is re-rendering */
-  //That is what useMemo lets u do -> across renders if you want to remember a value-> use Memo
- 
 
-  //we can use -> useMemo where you dont need a state variable only InMemory variable & depends on Input value
-  useEffect(() =>{ 
-   let finalCount = 0;
-  for (let i = 1; i <= inputValue; i++) {
-    finalCount = finalCount + i;
-  }
-  setCount(finalCount)
-},[inputValue])
-
-//slightly cleaner saves one State variable 
-  return <div>
-    <input onChange={function(e) {
-      setInputValue(e.target.value);
-    }} placeholder={"Find sum from 1 to n"}></input>
-    <br />
-    Sum from 1 to {inputValue} is {count}
-    <br />
+ return <div>
     <button onClick={() => {
       setCounter(counter + 1);
     }}>Counter ({counter})</button>
+    <Demo a={a} ></Demo>
   </div>
  
 }
+
+//memo -> only actually re-render if I pass something which has actually changed
+const Demo = memo(function({a}){
+  console.log("rerender")
+  return <div>
+    hi there
+  </div>
+})
 
 export default App;
