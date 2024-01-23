1ec8a15574b2b4b99a5c17053124eb014a186d2a
Rahul <prathampathak14@gmail.com>
2024-01-24 00:29:15 +0530

    added learnings05.txt and learnings06.txt

--- React 07/routing/src/store/atoms/count.jsx
+++ React 07/routing/src/store/atoms/count.jsx
@@ -1,6 +1,6 @@
 //define a single atom -> this is going to store my counter information
 
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 //dont have the same key for 2 different atoms
 
@@ -9,3 +9,11 @@ export const countAtom = atom({
     default:0
 });
 //default -> what do you want the specific value of this atom to be 
+
+export const evenSelector = selector({
+    key:"evenSelector",
+    get:({get})=>{
+        const count = get(countAtom)
+        return count % 2;
+    }
+})
